359a9a1cb1efe55f44cb1080738f50f82622428a
Anubhav Singh <anubhavsingh@Mac.lan>
2026-02-02 02:14:31 +0530

    fixed the deployment errors

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
+  webpack: (config, { isServer }) => {
+    // Handle Node.js modules that aren't available in the browser
+    config.resolve.fallback = { 
+      fs: false, 
+      net: false, 
+      tls: false,
+      crypto: false,
+    };
+
+    // Ignore optional dependencies that cause build issues
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'pino-pretty': false,
+      };
+    }
+
+    // Suppress warnings about optional dependencies
+    config.ignoreWarnings = [
+      { module: /node_modules\/pino/ },
+      { module: /node_modules\/pino-pretty/ },
+    ];
+
     return config;
   },
 }
